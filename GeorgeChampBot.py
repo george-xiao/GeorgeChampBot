4b41562468c6350e69e93c8c5702875a74679e6f
George Xiao <g5xiao@edu.uwaterloo.ca>
2020-09-24 20:50:34 -0400

    initial commit, weekly updates, !plscount, !leaderboard

---
+++ GeorgeChampBot.py
@@ -0,0 +1,192 @@
+# GeorgeChampBot.py
+
+import asyncio
+from collections import Counter
+from datetime import datetime
+import discord
+from dotenv import load_dotenv
+from emoji import UNICODE_EMOJI
+import math
+import operator
+import os
+import re
+import shelve
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+ANNOUNCEMENT_CHANNEL = os.getenv('ANNOUNCEMENT_CHANNEL')
+ANNOUNCEMENT_DAY = int(os.getenv('ANNOUNCEMENT_DAY'))
+ANNOUNCEMENT_HOUR = int(os.getenv('ANNOUNCEMENT_HOUR'))
+ANNOUNCEMENT_MIN = int(os.getenv('ANNOUNCEMENT_MIN'))
+
+client = discord.Client()
+s = shelve.open('weekly_georgechamp_shelf.db')
+s_all_time = shelve.open('all_time_georgechamp_shelf.db')
+
+
+def score_algorithm(emoji_count):
+    return 0.61 + (1.37 * math.log(emoji_count))
+
+
+def updateCounts(key, increment = 1):
+    try:
+        if s.get(key) is None:
+            s[key] = increment
+        else:
+            s[key] += increment
+
+        if s_all_time.get(key) is None:
+            s_all_time[key] = increment
+        else:
+            s_all_time[key] += increment
+
+        return True
+    except Exception:
+        return False
+
+
+def is_emoji(s):
+    return s in UNICODE_EMOJI
+
+
+async def announcement_task():
+    print("it rly should print :)")
+    guilds = client.guilds
+    channel = None
+    for guild in guilds:
+        if guild.name == GUILD:
+            for guild_channel in guild.channels:
+                if guild_channel.name == ANNOUNCEMENT_CHANNEL:
+                    # channel type = channel model
+                    channel = guild_channel
+
+    shelf_as_dict = dict(s)
+    most_used_emotes = dict(sorted(shelf_as_dict.items(), key=operator.itemgetter(1), reverse=True)[:5])
+
+    keys = []
+    key_vals = []
+    for key in most_used_emotes.keys():
+        keys.append(key)
+        key_vals.append(most_used_emotes[key])
+
+    leaderboard_msg = "Here's the weekly emote update! \nEmote - Score \n"
+    for i in range(5):
+        if (i < len(keys)):
+            leaderboard_msg = leaderboard_msg + str(i + 1) + ". " + keys[i] + " - " + str(key_vals[i]) + "\n"
+
+    await channel.send(leaderboard_msg)
+
+    s.clear()
+    s_all_time.clear()
+
+
+@client.event
+async def on_ready():
+    guilds = client.guilds
+    channel = ""
+    georgechamp_emoji = None
+    for guild in guilds:
+        if guild.name == GUILD:
+            for emoji in guild.emojis:
+                if 'georgechamp' in emoji.name:
+                    georgechamp_emoji = emoji
+
+            for guild_channel in guild.channels:
+                if guild_channel.name == ANNOUNCEMENT_CHANNEL:
+                    # channel type = channel model
+                    channel = guild_channel
+
+    msg = await channel.send("GeorgeChampBot reporting for duty!")
+    # assume only one emoji has georgechamp in it
+    await msg.add_reaction(georgechamp_emoji.name + ":" + str(georgechamp_emoji.id))
+
+    while 1:
+        # seconds/week
+        curr_date = datetime.now()
+        if curr_date.weekday() == ANNOUNCEMENT_DAY:
+            print("past day check")
+            if curr_date.hour == ANNOUNCEMENT_HOUR:
+                print("past hours check")
+                if curr_date.minute == ANNOUNCEMENT_MIN:
+                    print("past min check")
+                    await announcement_task()
+                    # wait 6 days, 23h, 30 mins
+                    await asyncio.sleep(603000)
+
+        await asyncio.sleep(30)
+
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return None
+    elif message.content.startswith('!plscount'):
+        try:
+            requested_emote = message.content[10:]
+            await message.channel.send(requested_emote + " has been used " + str(s_all_time[requested_emote]) + " times.")
+        except KeyError:
+            await message.channel.send("Looks like that emote hasn't been used yet.")
+        except IndexError:
+            await message.channel.send("Doesn't appear that you've added an emote, please add an emote to check.")
+    elif message.content.startswith('!leaderboard'):
+        shelf_as_dict = dict(s_all_time)
+        start = 0
+        end = 10
+
+        if len(message.content) != len('!leaderboard'):
+            increment = int(message.content[len('!leaderboard')+1:])
+            # page size = 10
+            start += (increment - 1) * 10
+            end += (increment - 1) * 10
+
+        most_used_emotes = dict(sorted(shelf_as_dict.items(), key=operator.itemgetter(1), reverse=True)[start:end])
+        keys = []
+        key_vals = []
+        for key in most_used_emotes.keys():
+            keys.append(key)
+            key_vals.append(most_used_emotes[key])
+
+        if len(keys) == 0:
+            await message.channel.send("Doesn't look like there are emojis here :( Try another page.")
+        else:
+            leaderboard_msg = "Here's the all time leaderboard! \nEmote - Score \n"
+            for i in range(10):
+                if (i < len(keys)):
+                    placement = start + i + 1
+                    leaderboard_msg = leaderboard_msg + str(placement) + ". " + keys[i] + " - " + str(key_vals[i]) + "\n"
+
+            await message.channel.send(leaderboard_msg)
+    else:
+        custom_emojis = re.findall(r'<:\w*:\d*>', message.content)
+
+        emoji_names = list(Counter(custom_emojis).keys())
+        emoji_counts = list(Counter(custom_emojis).values())
+        for i in range(len(emoji_names)):
+            updateCounts(emoji_names[i], round(score_algorithm(emoji_counts[i])))
+
+        unicode_emojis = []
+        for character in message.content:
+            if is_emoji(character):
+                unicode_emojis.append(character)
+
+        emoji_names = list(Counter(unicode_emojis).keys())
+        emoji_counts = list(Counter(unicode_emojis).values())
+
+        for i in range(len(emoji_names)):
+            updateCounts(emoji_names[i], round(score_algorithm(emoji_counts[i])))
+
+
+@client.event
+async def on_raw_reaction_add(payload):
+    if (payload.emoji.is_custom_emoji()):
+        reaction_emoji_key = "<:" + payload.emoji.name + ":" + str(payload.emoji.id) + ">"
+        updateCounts(reaction_emoji_key)
+    elif payload.emoji.is_unicode_emoji():
+        updateCounts(payload.emoji.name)
+
+
+client.run(TOKEN)
+s.close('weekly_georgechamp_shelf.db')
+s_all_time.close('all_time_georgechamp_shelf.db')
---
+++ README.md
@@ -0,0 +1,59 @@
+# GeorgeChampBot
+
+GeorgeChampBot, a discord bot that tracks emoji usage
+
+## Getting Started
+
+To get a copy of this project, install [git](https://git-scm.com/), and then run the following command in the terminal:
+
+```
+git clone https://github.com/george-xiao/GeorgeChampBot.git
+```
+
+Change your directory in the terminal to the cloned project:
+
+```
+cd your/path/here
+```
+
+Requirements:
+
+[discord.py](https://discordpy.readthedocs.io/en/latest/) - a modern, easy to use, feature-rich, and async ready API wrapper for Discord.  
+[dotenv](https://pypi.org/project/python-dotenv/) - reads the key-value pair from a .env file  
+[emoji](https://pypi.org/project/emoji/) - emojis for Python
+
+```
+pip install discord.py
+pip install python-dotenv
+pip install emoji
+```
+
+Environment:  
+  
+In .env, provide the bot token, discord, guild (server) name, announcement channel name (for weekly updates), and announcement time.
+
+Use the following command to start the bot:
+
+```
+python GeorgeChampBot.py
+```
+
+## Built With
+* [asyncio](https://docs.python.org/3/library/asyncio.html) - write concurrent code using the async/await syntax 
+* [collections](https://docs.python.org/3/library/collections.html) - specialized container datatypes providing alternatives to Python’s general purpose built-in containers
+* [datetime](https://docs.python.org/3/library/datetime.html) - classes for manipulating dates and times
+* [discord.py](https://discordpy.readthedocs.io/en/latest/) - API wrapper for Discord
+* [dotenv](https://pypi.org/project/python-dotenv/) - reads the key-value pair from .env file
+* [emoji](https://pypi.org/project/emoji/) - emojis for Python
+* [math](https://docs.python.org/3/library/math.html) - mathematical functions defined by the C standard
+* [operator](https://docs.python.org/3/library/operator.html) - set of efficient functions corresponding to the intrinsic operators of Python
+* [os](https://docs.python.org/3/library/os.html) - operating system dependent functionality
+* [re](https://docs.python.org/3/library/re.html) - regular expression matching operations
+* [shelve](https://docs.python.org/3/library/shelve.html) - a persistent, dictionary-like object
+
+## Authors
+
+* **George Xiao** - [george-xiao](https://github.com/george-xiao)
+* **Symoom Saad** - [PSYmoom](https://github.com/PSYmoom)
+* **Maaz Mazharul** - [mmaaz1](https://github.com/mmaaz1)
+* **Steven Aung**

792003b3977b60a16069f04693f83e33ebba0c1a
George Xiao <g5xiao@edu.uwaterloo.ca>
2020-10-04 23:10:32 -0400

    added find_channel method and added recent match queries w/ opendota api

--- GeorgeChampBot.py
+++ GeorgeChampBot.py
@@ -10,6 +10,7 @@ import math
 import operator
 import os
 import re
+import requests
 import shelve
 
 load_dotenv()
@@ -21,11 +22,20 @@ ANNOUNCEMENT_HOUR = int(os.getenv('ANNOUNCEMENT_HOUR'))
 ANNOUNCEMENT_MIN = int(os.getenv('ANNOUNCEMENT_MIN'))
 WELCOME_CHANNEL = os.getenv('WELCOME_CHANNEL')
 WELCOME_ROLE = os.getenv("WELCOME_ROLE")
+OPENDOTA_API_KEY = os.getenv('OPENDOTA_API_KEY')
+DOTA_CHANNEL = os.getenv("DOTA_CHANNEL")
+PLAYER_1_ID = os.getenv('PLAYER_1_ID')
+PLAYER_2_ID = os.getenv('PLAYER_2_ID')
+PLAYER_3_ID = os.getenv('PLAYER_3_ID')
+player_list = [PLAYER_1_ID, PLAYER_2_ID, PLAYER_3_ID]
+
 
 client = discord.Client()
 s = shelve.open('weekly_georgechamp_shelf.db')
 s_all_time = shelve.open('all_time_georgechamp_shelf.db')
+dota_shelf = shelve.open('dota_shelf.db')
 
+open_dota_players_url = "https://api.opendota.com/api/players/"
 
 def score_algorithm(emoji_count):
     return 0.61 + (1.37 * math.log(emoji_count))
@@ -52,16 +62,20 @@ def is_emoji(s):
     return s in UNICODE_EMOJI
 
 
-async def announcement_task():
-    print("it rly should print :)")
+async def find_channel(channel_name):
     guilds = client.guilds
-    channel = None
     for guild in guilds:
         if guild.name == GUILD:
             for guild_channel in guild.channels:
-                if guild_channel.name == ANNOUNCEMENT_CHANNEL:
+                if guild_channel.name == channel_name:
                     # channel type = channel model
-                    channel = guild_channel
+                    return guild_channel
+
+
+async def announcement_task():
+    print("it rly should print :)")
+    guilds = client.guilds
+    channel = await find_channel(ANNOUNCEMENT_CHANNEL)
 
     shelf_as_dict = dict(s)
     most_used_emotes = dict(sorted(shelf_as_dict.items(), key=operator.itemgetter(1), reverse=True)[:5])
@@ -69,7 +83,7 @@ async def announcement_task():
     keys = []
     key_vals = []
     for key in most_used_emotes.keys():
-        keys.append(key)
+        keys.appendp(key)
         key_vals.append(most_used_emotes[key])
 
     leaderboard_msg = "Here's the weekly emote update! \nEmote - Score \n"
@@ -81,6 +95,31 @@ async def announcement_task():
 
     s.clear()
     s_all_time.clear()
+
+
+async def check_recent_matches():
+    curr_epoch_time = int(datetime.now().timestamp())
+    channel = await find_channel(DOTA_CHANNEL)
+    match_ids = []
+    dota_shelf_as_dict = dict(dota_shelf)
+    hed = {'Authorization': 'Bearer ' + OPENDOTA_API_KEY}
+    for player in player_list:
+        try:
+            res = requests.get(open_dota_players_url + player + '/recentMatches', headers=hed)
+            if res.status_code == 200:
+                recent_matches = res.json()
+                for match in recent_matches:
+                    # if game in last ~15 minutes, extra 10s just in case something breaks idk
+                    if curr_epoch_time - (match['start_time'] + match['duration']) < 910:
+                        match_ids.append(match['match_id'])
+
+                for match_id in match_ids:
+                    if dota_shelf.get(match_id) is None:
+                        await channel.send("Looks like someone played a game... Here's the match:\nhttps://www.dotabuff.com/matches/" + str(match_id))
+                        dota_shelf[match_id] = 1
+        except Exception:
+            print("uh oh, error")
+
 
 
 @client.event
@@ -106,31 +145,19 @@ async def on_ready():
     while 1:
         # seconds/week
         curr_date = datetime.now()
-        if curr_date.weekday() == ANNOUNCEMENT_DAY:
-            print("past day check")
-            if curr_date.hour == ANNOUNCEMENT_HOUR:
-                print("past hours check")
-                if curr_date.minute == ANNOUNCEMENT_MIN:
-                    print("past min check")
-                    await announcement_task()
-                    # wait 6 days, 23h, 30 mins
-                    await asyncio.sleep(603000)
+        # if announcement time, assume it'll be on the hour e.g. 9:00am
+        if curr_date.weekday() == ANNOUNCEMENT_DAY and curr_date.hour == ANNOUNCEMENT_HOUR and curr_date.minute == ANNOUNCEMENT_MIN:
+            await announcement_task()
 
-        await asyncio.sleep(30)
+        elif curr_date.minute == 32:
+            await check_recent_matches()
 
+        await asyncio.sleep(55)
 
 @client.event
 async def on_member_join(member):
-    channel = ""
+    channel = await find_channel(WELCOME_CHANNEL)
     try:
-        guilds = client.guilds
-        for guild in guilds:
-            if guild.name == GUILD:
-                for guild_channel in guild.channels:
-                    if guild_channel.name == WELCOME_CHANNEL:
-                        # channel type = channel model
-                        channel = guild_channel
-
         await member.add_roles(discord.utils.get(member.guild.roles, name=WELCOME_ROLE))
     except Exception as e:
         await channel.send('There was an error running this command ' + str(e))  # if error
@@ -140,16 +167,8 @@ async def on_member_join(member):
 
 @client.event
 async def on_disconnect():
-    channel = ""
+    channel = await find_channel(WELCOME_CHANNEL)
     try:
-        guilds = client.guilds
-        for guild in guilds:
-            if guild.name == GUILD:
-                for guild_channel in guild.channels:
-                    if guild_channel.name == WELCOME_CHANNEL:
-                        # channel type = channel model
-                        channel = guild_channel
-
         await channel.send("GeorgeChampBot signing out.")
     except Exception:
         await channel.send("I believe I am leaving but something went wrong... Blame George.")

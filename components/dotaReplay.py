e0aca441ed78047ada19cc65bcda11941409be52
Mazharul Maaz <maazmazharul02@gmail.com>
2021-12-19 19:46:47 -0500

    Change in comment

--- components/dotaReplay.py
+++ components/dotaReplay.py
@@ -29,7 +29,7 @@ async def check_recent_matches(channel, player_list, OPENDOTA_API_KEY):
         s.close()
         match_ids = list(set(match_ids))
         for match_id in match_ids:
-            #delete match history after 1.5 days
+            #delete match history after 1 day
             await channel.send(
                 "Looks like someone played a game... Here's the match:\nhttps://www.dotabuff.com/matches/" + str(
                 match_id), delete_after=86400)

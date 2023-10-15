1958a3b95461dd4b1f600927b7d8b33d0de446f8
Symoom Saad <51514828+PSYmoom@users.noreply.github.com>
2023-10-15 18:24:10 -0400

    Meme Review Fix (#38)
    Fixed Meme of the Week

--- Dockerfile
+++ Dockerfile
@@ -4,16 +4,17 @@ FROM ubuntu:latest
 RUN apt-get update
 RUN apt-get install -y python3 python3-dev python3-pip
 RUN apt-get install -y ffmpeg
+RUN apt-get install -y python3-gdbm
+
+# Install Python dependencies
+COPY ./requirements.txt ./
+RUN pip3 install -r requirements.txt
 
 # Copy source files
 COPY ./.env ./
 COPY ./GeorgeChampBot.py ./
 COPY ./common ./common
 COPY ./components ./components
-COPY ./requirements.txt ./
-
-# Install Python dependencies
-RUN pip3 install -r requirements.txt
 
 # Run GeorgeChampBot
 CMD ["python3", "./GeorgeChampBot.py"]
--- components/memeReview.py
+++ components/memeReview.py
@@ -10,10 +10,23 @@ ehMeme = 'one'
 goodMeme = 'two'
 bestMeme = 'three'
 
-def isMeme(attachments):
-    formatList = ["jpg", "jpeg", "JPG", "JPEG", "png", "PNG", "gif", "gifv", "webm", "mp4", "wav", "mov"]
-    fileFormat = attachments.url.split(".")[-1]
-    return fileFormat in formatList
+def isMeme(attachment: discord.Attachment) -> bool:
+    return is_image(attachment.url) or is_video(attachment.url)
+
+def is_image(attachment_url: str) -> bool:
+    formatList = ["jpg", "jpeg", "JPG", "JPEG", "png", "PNG", "gif", "gifv"]
+    fileFormat = get_attachment_format(attachment_url)
+    return any(fileFormat.startswith(format) for format in formatList)
+
+def is_video(attachment_url: str) -> bool:
+    formatList = ["webm", "mp4", "wav", "mov"]
+    fileFormat = get_attachment_format(attachment_url)
+    return any(fileFormat.startswith(format) for format in formatList)
+
+def get_attachment_format(attachment_url: str) -> bool:
+    fileUrl = attachment_url.split("?")[0]
+    fileFormat = fileUrl.split(".")[-1]
+    return fileFormat
 
 def getUser(message):
     return message.embeds[0].description[2:-1]
@@ -39,7 +52,7 @@ async def check_meme(message, guild, channel, memeChannel):
 
         embed = discord.Embed(description='<@' + str(message.author.id) + '>')
         embed.set_image(url=message.attachments[0].url)
-        if message.attachments[0].url[-4:] in ["webm", ".mp4", ".wav", ".mov"]:
+        if is_video(message.attachments[0].url):
             await memeChannel.send(content=message.attachments[0].url)
         memeMessage = await memeChannel.send(embed=embed)
         
@@ -295,7 +308,7 @@ async def best_announcement_task(channel, deleteAfter=None):
             sendMsg += "Third Place: :third_place:" + "<@" + str(sorted_database[2][1][2]) + "> " + "\n"
         embed = discord.Embed(title="Best Meme:")
         embed.set_image(url=sorted_database[0][1][3])
-        if sorted_database[0][1][3][-4:] in ["webm", ".mp4", ".wav", ".mov"]:
+        if is_video(sorted_database[0][1][3]):
             sendMsg += "Best Meme:\n" + sorted_database[0][1][3]
             await channel.send(content=sendMsg, delete_after=deleteAfter)
         else:
@@ -306,7 +319,7 @@ async def best_announcement_task(channel, deleteAfter=None):
         memeDatabase.close()
         memeLeaderboard.close()
     except Exception as e:
-        await channel.send('Error Printing Best Emote: ' + str(e))
+        await channel.send('Error Printing Best Meme of the Week: ' + str(e))
       
         
 async def print_memerboard(message):

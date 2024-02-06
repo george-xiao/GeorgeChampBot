ee582010433acee8533c4595ab0204d76b3abd93
PSYmoom <saadsymoom@gmail.com>
2024-02-05 20:39:04 -0500

    Convert suggestion list to be ordered

---
+++ common/orderedShelve.py
@@ -0,0 +1,30 @@
+'''
+Workaround to achieve ordered shelve; a dict is stored under db["dict"]
+Why is this workaround needed?
+    Note that dbm implements a dict using internal hash-function that does not keep track of insertion order
+    Since shelves is implemented using dbm, insertion order is not maintained
+    To maintain insertion order, a dict is pickled in the shelve
+How does it work?
+    Since dict maintains insertion order in Python3.7+, we can use it to bypass having to maintain order ourselves
+TODO: Find a non-hacky solution
+'''
+import shelve
+
+class OrderedShelve:
+    def __init__(self, database_path: str):
+        self.shelve_db = None
+        self.database_path = database_path
+
+    def open(self) -> dict:
+        self.shelve_db = shelve.open(self.database_path)
+        db_dict = self.shelve_db["dict"]
+        if not db_dict:
+            return {}
+        return db_dict
+
+    def close(self, modified_dict: dict):
+        self.__modify(modified_dict)
+        self.shelve_db.close()
+
+    def __modify(self, modified_dict: dict):
+        self.shelve_db["dict"] = modified_dict
